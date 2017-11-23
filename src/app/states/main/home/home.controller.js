18769de893632b3272b76a0239da0fb480cc9155
wesley <wesleyyw8@gmail.com>
2017-11-23 00:21:59 +0000

    duplicated line removed

--- src/app/states/main/home/home.controller.js
+++ src/app/states/main/home/home.controller.js
@@ -2,18 +2,22 @@
 
 import { createUser } from 'data/users/index.js';
 
-const homeController = ($scope) => {
-  //$scope.form = {};
+const homeController = ($scope, $timeout) => {
+  $scope.form = {};
   $scope.submitForm = () => {
     createUser({
       firstname: $scope.form.firstName,
       lastname: $scope.form.lastName,
       email: $scope.form.email
-    }).then((resp) => {
+    }).then(() => {
       alert('user created successfully');
+      $timeout(() => {
+        $scope.form = {};
+        $scope.apply();
+      }, 0);
     });
   };
 }
-homeController.$inject = ['$scope'];
+homeController.$inject = ['$scope', '$timeout'];
 
 module.exports = homeController;
